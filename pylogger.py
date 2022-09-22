1bab908af60e43ce76ca1e65cdf12928cdb67a0c
Adam Traub <adamtraub@Adams-MBP.lan>
2022-09-22 15:59:28 -0400

    created a proper settings file, the logger is much stronger now

--- pylogger.py
+++ pylogger.py
@@ -3,45 +3,16 @@ from colors import colors
 from datetime import datetime
 from enum import Enum
 import pytz
+import settings
 
 class Log():
     def __init__(self, identifier:str):
         self.identifier = identifier
-        # TODO: Move settings to a seperate file so that all of this stuff can be customized
-        self.modes = Enum('Mode', 'INFO VERBOSE WARN ERROR FATAL')
-        self.settings = {
-            self.modes.INFO: {
-                'active': True,
-                'text_color' : colors.fg.green,
-            },
-            self.modes.VERBOSE: {
-                'active':True,
-                'text_color' : colors.fg.purple,
-            },
-            self.modes.WARN: {
-                'active':True,
-                'text_color' : colors.fg.yellow,
-            },
-            self.modes.ERROR: {
-                'active':True,
-                'text_color' : colors.fg.red,
-            },
-            self.modes.FATAL: {
-                'active':True,
-                'identifier_text_color': colors.fg.red,
-                'text_color' : colors.fg.red,
-                'time_text_color': colors.fg.red,
-            },
-            'identifier_text_color': colors.fg.blue,
-            'output_prefix_format': '[{timestamp}][{identifier}]',
-            'output_format': '{prefix} {text}',
-            'timezeone': pytz.timezone("US/Eastern"),
-            'time_format':'%H:%M',
-            'time_text_color': colors.fg.red,
-            'text_color': colors.fg.darkgrey,
-        }
+        self.modes = settings.modes
+        self.settings = settings.config
+        self._setup()
 
-    def log(self, text, mode):
+    def _log(self, text, mode):
         """This should only be called directly with custom modes - coming soon"""
         if self.settings.get(mode).get('active',False):
             text = colors.color_text(text, fg_color=self.settings.get(mode).get('text_color',self.settings.get('text_color', '')))
@@ -60,46 +31,21 @@ class Log():
         identifier = colors.color_text(text=self.identifier, fg_color=identifier_text_color, bg_color=identifier_bg_color)
         return self.settings.get('output_prefix_format','{timestamp}|{identifier}').format(identifier=identifier,timestamp=timestamp)
 
-    # def setup(self):
-    #     new_funcs = {}
-    #     for mode in self.modes:
-    #         name = str(mode).split('.')[-1].lower()
-    #         new_funcs[name] = lambda self,text: self.log(text=text, mode=mode)
-    #         setattr(self.__class__, name, new_funcs[name])
-    #     print(new_funcs)
-    #     new_funcs['info'](self,'test')
-
-    def func_factory(self, mode):
+    def _func_factory(self, mode):
         def f(self, text):
-            self.log(text=text, mode=mode)
+            self._log(text=text, mode=mode)
         return f
 
-    def setup(self):
+    def _setup(self):
         for mode in self.modes:
             name = str(mode).split('.')[-1].lower()
-            new_func = self.func_factory(mode)
+            new_func = self._func_factory(mode)
             setattr(self.__class__, name, new_func)
 
-
-    # def info(self, text):
-    #     self.log(text=text, mode=self.modes.INFO)
-
-    # def warn(self, text):
-    #     self.log(text=text, mode=self.modes.WARN)
-
-    # def verbose(self, text):
-    #     self.log(text=text, mode=self.modes.VERBOSE)
-
-    # def error(self, text):
-    #     self.log(text=text, mode=self.modes.ERROR)
-
-    # def fatal(self, text):
-    #     self.log(text=text, mode=self.modes.FATAL)
-
-logger = Log('KINC')
-logger.setup()
-logger.info('info!')
-logger.warn(f"warning!")
-logger.verbose("Verbose text")
-logger.error('Error')
-logger.fatal("FATAL")
+if __name__ == "__main__":
+    logger = Log('KINC')
+    logger.info('info!')
+    logger.warn(f"warning!")
+    logger.verbose("Verbose text")
+    logger.error('Error')
+    logger.fatal("FATAL")
