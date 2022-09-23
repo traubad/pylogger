a0723f4466be0dddf82c22cec020abfba541471c
Adam Traub <traubad@vcu.edu>
2022-09-23 12:35:23 -0400

    hopefully fixed build

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='pylogger',
-    packages=find_packages(include=['pylogger']),
+    packages=find_packages(include=['pylogger','_colors','_pylogger_settings.py']),
     version='0.1.0',
     description='A simple yet powerful library for logging',
     author='Adam',
