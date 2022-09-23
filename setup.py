34a9f937a82de6922f3b50d53d0a23843b5443bc
Adam Traub <traubad@vcu.edu>
2022-09-23 12:22:28 -0400

    updated to require modules

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import find_packages, setup
 
 setup(
     name='pylogger',
-    packages=find_packages(include=['pylogger']),
+    packages=find_packages(include=['pylogger','./modules/*']),
     version='0.1.0',
     description='A simple yet powerful library for logging',
     author='Adam',
     license='GNU',
-    install_requires=[],
+    install_requires=['pytz',''],
 )
